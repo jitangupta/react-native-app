6d76bc0aa0a41367bc4798b4e30f7a498e1db045
Jitan Gupta <gtangupta@gmail.com>
2022-05-03 16:05:01 +0530

    configured icons

--- src/App.js
+++ src/App.js
@@ -12,12 +12,13 @@ import {
   Text,
   View,
 } from 'react-native';
-
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <SafeAreaView>
       <View>
+        <Icon name="home" size={30} />
         <Text>React Native App</Text> 
       </View>
     </SafeAreaView>
